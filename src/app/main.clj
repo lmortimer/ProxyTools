13a082072030d85b2e6240f235feb5c13e4de340
lastkarrde <last.karrde@gmail.com>
2012-06-11 12:42:39 +1200

    runs all with lein run

--- src/app/main.clj
+++ src/app/main.clj
@@ -1,13 +1,13 @@
 (ns app.main
-	(:use [websocket.main :only [start-websocket-server]]))
+	(:use  	[ring.adapter.jetty :only [run-jetty]]
+			[websocket.main :only [start-websocket-server]]
+			[website.main :only [app]]))
 
 (defn -main
   []
+  	(run-jetty #'app {:port 80 :join? false})
     (start-websocket-server)
-
-
-
-
+    
 
 
 	)
