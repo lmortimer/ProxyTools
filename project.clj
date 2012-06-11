8f2ec2833b80340ca520817549d40d425a6c48da
lastkarrde <last.karrde@gmail.com>
2012-06-11 19:20:15 +1200

    add dependency for ring. need to be explicit since compojure doesnt load them all

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-proxytools "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+  				 [ring "1.1.0"]
                  [compojure "1.0.4"]
                  [clj-json "0.5.0"]
                  [enlive "1.0.0"]
