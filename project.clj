bdf1fabd67b8d935a0e86b95a449783e506a7b44
Logan <last.karrde@gmail.com>
2012-07-21 19:37:45 +1200

    gitignore

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject clj-proxytools "1.0.0-SNAPSHOT"
   :description "SOCKS and HTTP Proxy Checker"
   :dependencies [[org.clojure/clojure "1.3.0"]
-  				 [ring "1.1.0"]
+  				       [ring "1.1.0"]
                  [compojure "1.0.4"]
                  [clj-json "0.5.0"]
                  [enlive "1.0.0"]
-                 [aleph "0.3.0-alpha2"]]
+                 [aleph "0.3.0-alpha2"]
+                 [clj-geoip "0.1.1"]]
   :plugins      [[lein-ring "0.7.0"]]
   :main app.main
   :ring {:handler website.main/app})
